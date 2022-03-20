9b6627bf9676ff0e2dbbdae660df59696b5a4b79
Weijing Wang <weijingrocks@gmail.com>
2022-03-20 14:13:46 -0700

    lanes into polygons

--- pyweek33.py
+++ pyweek33.py
@@ -1,5 +1,6 @@
-from pickle import FALSE
 import pygame
+import pygame.gfxdraw
+
 pygame.init()
 done = False
 
@@ -32,12 +33,24 @@ class lane:
 
 
 
-
+lanes = [
+    [(focal_point,horizon_line),[x,screen_height],[x+road_width,screen_height]],
+    [(focal_point,horizon_line),[x,screen_height],[x-road_width,screen_height]]
+]
 
 
 
 
 while not done:
+    lanes = [
+        [(focal_point,horizon_line),[x,screen_height],[x+road_width,screen_height]],
+        [(focal_point,horizon_line),[x,screen_height],[x-road_width,screen_height]],
+        [(focal_point,horizon_line),[x+road_width,screen_height],[x+road_width*2,screen_height]],
+        [(focal_point,horizon_line),[x+road_width*2,screen_height],[x+road_width*3,screen_height]],
+        [(focal_point,horizon_line),[x-road_width,screen_height],[x-road_width*2,screen_height]],
+        [(focal_point,horizon_line),[x-road_width*2,screen_height],[x-road_width*3,screen_height]]
+        
+    ]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
@@ -49,23 +62,25 @@ while not done:
 
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_RIGHT]: 
-        accel_x+=0.3
-        x += 10+accel_x
-    if pressed[pygame.K_LEFT]:
-        accel_x+=0.3
+        accel_x+=0.1
         x -= 10+accel_x
+    if pressed[pygame.K_LEFT]:
+        accel_x+=0.1
+        x += 10+accel_x
 
 
     screen.fill("black")
-    pygame.draw.line(screen,"white",(focal_point,horizon_line),(x,screen_height),5)
-    pygame.draw.line(screen,"white",(focal_point,horizon_line),(x+road_width,screen_height),5)
-    pygame.draw.line(screen,"white",(focal_point,horizon_line),(x-road_width,screen_height),5)
-    pygame.draw.line(screen,"white",(focal_point,horizon_line),(x+road_width*2,screen_height),5)
-    pygame.draw.line(screen,"white",(focal_point,horizon_line),(x-road_width*2,screen_height),5)
-    pygame.draw.line(screen,"white",(focal_point,horizon_line),(x+road_width*3,screen_height),5)
-    pygame.draw.line(screen,"white",(focal_point,horizon_line),(x-road_width*3,screen_height),5)
-    pygame.draw.line(screen,"white",(focal_point,horizon_line),(x+road_width*4,screen_height),5)
-    pygame.draw.line(screen,"white",(focal_point,horizon_line),(x-road_width*4,screen_height),5)   
+    for lane in lanes:
+        pygame.gfxdraw.filled_polygon(screen,lane,(lane[2][0]%255,lane[1][0]%255,lane[2][0]%255))
+    # pygame.draw.line(screen,"white",(focal_point,horizon_line),(x,screen_height),5)
+    # pygame.draw.line(screen,"white",(focal_point,horizon_line),(x+road_width,screen_height),5)
+    # pygame.draw.line(screen,"white",(focal_point,horizon_line),(x-road_width,screen_height),5)
+    # pygame.draw.line(screen,"white",(focal_point,horizon_line),(x+road_width*2,screen_height),5)
+    # pygame.draw.line(screen,"white",(focal_point,horizon_line),(x-road_width*2,screen_height),5)
+    # pygame.draw.line(screen,"white",(focal_point,horizon_line),(x+road_width*3,screen_height),5)
+    # pygame.draw.line(screen,"white",(focal_point,horizon_line),(x-road_width*3,screen_height),5)
+    # pygame.draw.line(screen,"white",(focal_point,horizon_line),(x+road_width*4,screen_height),5)
+    # pygame.draw.line(screen,"white",(focal_point,horizon_line),(x-road_width*4,screen_height),5)   
 
     clock.tick(60)
     pygame.display.update()
