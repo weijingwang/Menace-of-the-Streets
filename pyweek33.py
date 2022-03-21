46b8aa71505f42ce46fa0bfc4394ae805dbb0a7b
Weijing Wang <weijingrocks@gmail.com>
2022-03-21 11:02:57 -0700

    add fog

--- pyweek33.py
+++ pyweek33.py
@@ -10,12 +10,12 @@ class Lane():
 class Game():
     def __init__(self,current_lane,screen):
         self.lane_array = [
-            [0,1,(33,33,33)],
+            [0,1,(33,83,33)],
             [1,2,(66,66,66)],
             [2,3,(99,99,99)],
             [3,4,(132,132,132)],
             [4,5,(165,165,165)],
-            [5,6,(198,198,198)]
+            [5,6,(198,248,198)]
         ]
         self.current_lane =current_lane
         self.screen_height = 720
@@ -28,6 +28,7 @@ class Game():
         self.x = self.current_lane*-self.road_width
         self.next_x = self.current_lane*-self.road_width
         self.screen = screen
+
 
     def update_lane(self):
         for event in pygame.event.get():
@@ -54,12 +55,18 @@ class Game():
             else:
                 self.x=self.next_x
                 self.move_left=False
-        return(self.current_lane,self.x,self.next_x)
+        if self.current_lane==-1 or self.current_lane ==6:
+            print("YOU DIE")
+
+        print(self.current_lane,self.x,self.next_x)
     def run(self):
+        # random_x = randrange(-3,3)
+        # random_y = randrange(-3,3)
         for lane in self.lane_array:
             pygame.gfxdraw.filled_polygon(self.screen,[(self.x0,self.y0),[self.x+self.road_width*lane[0],self.screen_height],[self.x+self.road_width*lane[1],self.screen_height]],lane[2])
-
-        # self.screen.blit(pygame.transform.scale(POV_car,(screen_width+random_x,screen_height+random_y)),(0,0))
+        self.screen.blit(pygame.transform.scale(fog,(screen_width,screen_height)),(0,0))
+        pygame.draw.rect(self.screen,"black",(0,620,1280,100))
+        self.screen.blit(pygame.transform.scale(POV_car,(screen_width,screen_height)),(0,0))
 
 
 
@@ -73,7 +80,7 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 clock= pygame.time.Clock()
  
 POV_car = pygame.image.load("./assets/POV_car.png").convert_alpha()
-
+fog = pygame.image.load("./assets/fog.png").convert_alpha()
 done = False
 
 # pygame.mixer.music.load("./assets/i drivin and they hatin.mp3")
